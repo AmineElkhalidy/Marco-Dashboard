d0f60a3e3229a864129e4857f7cd6e512d899146
Amine Elkhalidy <amineelkhalidy.a@gmail.com>
2023-04-14 16:05:27 +0000

    Pushing the changes...

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,13 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        textColor: "#000223",
+        bgColor: "#ECEFF5",
+      },
+    },
   },
   plugins: [],
-}
-
+};
